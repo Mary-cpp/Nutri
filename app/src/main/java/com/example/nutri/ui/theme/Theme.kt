0207a0f71a6efe1528c0157bb6418b552d9bbecd
MarySoproniuk <f13x@rasengan.ru>
2023-03-07 19:10:19 +0300

    Added main colors to Theme.kt

--- app/src/main/java/com/example/nutri/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nutri/ui/theme/Theme.kt
@@ -18,21 +18,23 @@ import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = Turquoise,
+    secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = LightGreen,
-    secondary = Green2,
-    tertiary = Pink40,
+    primary = DarkGreen,
+    secondary = Pink,
+    tertiary = BlueGrey,
 
-    surface = LightGreen,
-    onPrimary = MainGreen,
-    onSecondary = Green,
-    onTertiary = Turquoise,
-    onBackground = Color.White,
-    onSurface = Color(0xFF1C1B1F),
+    background = Color.White,
+    surface = LightGrey,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color.Black,
+    onSurface =Color.Black,
+    surfaceVariant = DarkerGrey
 
 )
 
