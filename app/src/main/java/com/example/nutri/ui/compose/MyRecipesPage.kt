430fa78e5da14b7136c93aacf6875f82d65748f9
Mary Soproniuk <m.soproniuk@gmail.com>
2023-03-10 13:45:11 +0300

    Finished up RecipeScreen design and added Bottom Sheet

--- app/src/main/java/com/example/nutri/ui/compose/MyRecipesPage.kt
+++ app/src/main/java/com/example/nutri/ui/compose/MyRecipesPage.kt
@@ -37,11 +37,11 @@ fun MyRecipesPage(vm: RecipeAnalyzeViewModel){
     Scaffold(modifier = Modifier.fillMaxSize(),
         topBar = { TopAppBar(title = { Text(text = "MyRecipesPage", color = Color.Black)},
         backgroundColor = MaterialTheme.colors.background, elevation = 0.dp) },
-        floatingActionButton = {FAB()},
-        content = {
+        floatingActionButton = {RecipeFAB()},
+        content = { paddingValues ->
             Surface(modifier = Modifier
                 .fillMaxSize()
-                .padding(it), color = MaterialTheme.colors.background){
+                .padding(paddingValues), color = MaterialTheme.colors.background){
 
                 Column(Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally){
@@ -68,7 +68,7 @@ fun MyRecipesPage(vm: RecipeAnalyzeViewModel){
 }
 
 @Composable
-fun FAB(){
+fun RecipeFAB(){
     FloatingActionButton(onClick = { /*TODO(Add recipe)*/},
         modifier = Modifier.size(48.dp),
 
@@ -93,31 +93,31 @@ fun SortAndFilter(){
         Button(onClick = { /*TODO(Sort)*/ },
             colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondaryVariant),
             shape = RoundedCornerShape(24.dp),
-            elevation = ButtonDefaults.elevation((-4).dp)
-        ) {
+            elevation = ButtonDefaults.elevation(4.dp),
+            content = {
+                Icon(imageVector = ImageVector.vectorResource(id = R.drawable.sort48px),
+                    tint = Color.Black,
+                    contentDescription = "SortIcon",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(end = 8.dp))
 
-            Icon(imageVector = ImageVector.vectorResource(id = R.drawable.sort48px),
-                tint = Color.Black,
-                contentDescription = "SortIcon",
-                modifier = Modifier
-                    .size(24.dp)
-                    .padding(start = 16.dp, end = 8.dp))
-
-            Text(text = "Sort", color = Color.Black, modifier = Modifier.padding(end = 16.dp))
-        }
+                Text(text = "Sort", color = Color.Black, modifier = Modifier.padding(end = 16.dp))
+            }
+        )
 
         Button(onClick = { /*TODO(Filter)*/ },
             colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
             shape = RoundedCornerShape(24.dp),
-            elevation = ButtonDefaults.elevation((-4).dp)
+            elevation = ButtonDefaults.elevation(4.dp)
         ) {
 
             Icon(imageVector = ImageVector.vectorResource(id = R.drawable.filter_alt48px),
                 tint = Color.Black,
                 contentDescription = "SortIcon",
                 modifier = Modifier
-                    .size(24.dp)
-                    .padding(start = 16.dp, end = 8.dp))
+                    .size(32.dp)
+                    .padding(end = 8.dp))
 
             Text(text = "Filter", color = Color.Black)
         }
--- app/src/main/java/com/example/nutri/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nutri/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.example.nutri.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
@@ -10,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
@@ -36,7 +34,6 @@ private val LightColorScheme = lightColors(
 fun NutriTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
