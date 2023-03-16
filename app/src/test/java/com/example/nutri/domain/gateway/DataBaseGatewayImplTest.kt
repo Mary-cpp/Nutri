7b76f829f57e3eb369cbc44797cb09f0730fe7fa
MarySoproniuk <m.soproniuk@gmail.com>
2023-03-16 16:10:52 +0300

    Tried to write some unit tests using Mockito

---
+++ app/src/test/java/com/example/nutri/domain/gateway/DataBaseGatewayImplTest.kt
@@ -0,0 +1,105 @@
+package com.example.nutri.domain.gateway
+
+import com.example.nutri.data.database.RecipeDatabase
+import com.example.nutri.data.database.dao.RecipeDAO
+import com.example.nutri.data.dto.Characteristics
+import com.example.nutri.domain.model.Recipe
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.Mockito
+import java.util.*
+
+internal class DataBaseGatewayImplTest {
+
+    private val database = Mockito.mock(RecipeDatabase::class.java)
+    private val dao = Mockito.mock(RecipeDAO::class.java)
+    private var name = ""
+
+
+    @BeforeEach
+    internal fun setUp() {
+
+        Mockito.`when`(database.recipeDAO()).thenReturn(dao)
+
+        Mockito.`when`(dao.getRecipeId(name)).thenReturn(Random().nextInt(100))
+        Mockito.`when`(dao.getIngredientId(name)).thenReturn(Random().nextInt(100))
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        database.close()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 24, 5])
+    fun saveSpecifiedIngredients(argument: Int) {
+
+        Mockito.`when`(dao.addIngredientsOfRecipe(emptyList())).thenReturn(Unit)
+
+        val expected = argument
+
+        val actual = DataBaseGatewayImpl(database)
+            .saveSpecifiedIngredients(
+                Random().nextInt(),
+                makeListOfIngredients(argument))
+
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 4, 50, 100])
+    fun saveLabels(argument: Int) {
+        Mockito.`when`(dao.addLabels(listOf())).thenReturn(Unit)
+
+        val expected = argument*3
+        val actual = DataBaseGatewayImpl(database)
+            .saveLabels(createRecipeWithLabels(argument), Random().nextInt(10))
+
+        assertEquals(expected, actual)
+    }
+
+    private fun makeListOfIngredients(size : Int) : List<Characteristics>{
+
+        val list : MutableList<Characteristics> = mutableListOf()
+
+        repeat(size){
+            list.add(createRandomIngredient())
+        }
+        return list.toList()
+    }
+
+
+    private fun createRecipeWithLabels(size: Int)
+    = Recipe(dietLabels = createLabels(size),
+    healthLabels = createLabels(size),
+        cautions = createLabels(size))
+
+
+    private fun createLabels(size: Int) : List<String>{
+
+        val list : MutableList<String> = mutableListOf()
+
+        repeat(size){
+            list.add("LABEL")
+        }
+        return list.toList()
+    }
+
+    private fun createRandomIngredient() : Characteristics{
+        return Characteristics(
+            quantity = Random().nextDouble(),
+            measure = "MEASURE",
+            foodMatch = "FOOD MATCH",
+            food = "FOOD",
+            foodId = "FOODiD",
+            weight = Random().nextDouble(),
+            retainedWeight =  Random().nextDouble(),
+            nutrients = null,
+            measureUri = "MEASURE URI",
+            status = "STATUS"
+        )
+    }
+}
