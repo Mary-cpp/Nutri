2aa6fb0a4006d7a7037714b474f6109f0525073f
MarySoproniuk <m.soproniuk@gmail.com>
2023-03-26 12:40:36 +0300

    Finished SearchPage.kt

--- app/src/main/java/com/example/nutri/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/example/nutri/ui/screens/search/SearchViewModel.kt
@@ -4,23 +4,24 @@ import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.nutri.domain.recipes.interactor.LocalRecipesInteractor
 import com.example.nutri.domain.recipes.model.Recipe
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    val useCase: LocalRecipesInteractor
+    private val useCase: LocalRecipesInteractor
 ) : ViewModel() {
 
     private val tag = "SearchViewModel"
 
     val foundRecipes : MutableState<List<Recipe>> = mutableStateOf(listOf())
 
-    fun getRecipes(name: String) = viewModelScope.launch{
+    fun getRecipes(name: String) = CoroutineScope(Dispatchers.Main).launch{
 
         Log.d(tag, "getRecipes     START")
 
@@ -28,7 +29,11 @@ class SearchViewModel @Inject constructor(
 
         listOfRecipes?.let{
             foundRecipes.value = listOfRecipes
+
+
         }
+
+        Log.d(tag, "Recipe: ${foundRecipes.value[0].name}")
 
         Log.d(tag, "getRecipes     END")
     }
