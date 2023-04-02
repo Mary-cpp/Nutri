9769bfe54dbac0118d95bf68f5efefcad9f7e374
Mary Soproniuk <m.soproniuk@gmail.com>
2023-04-02 14:01:00 +0300

    fix(room): added unique modifier to "name" column in IngredientEntity.kt updated RecipeDatabase.kt version

--- app/src/main/java/com/example/nutri/data/database/RecipeDatabase.kt
+++ app/src/main/java/com/example/nutri/data/database/RecipeDatabase.kt
@@ -32,7 +32,7 @@ import com.example.nutri.data.statistics.entities.RecipeInMeal
         MealCategory::class,
         RecipeInMeal::class
     ],
-    version = 17,
+    version = 18,
     exportSchema = false)
 @TypeConverters(Converter::class)
 abstract class RecipeDatabase : RoomDatabase() {
--- app/src/main/java/com/example/nutri/data/recipe/local/entity/IngredientEntity.kt
+++ app/src/main/java/com/example/nutri/data/recipe/local/entity/IngredientEntity.kt
@@ -1,9 +1,11 @@
 package com.example.nutri.data.recipe.local.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "ingredients")
+@Entity(tableName = "ingredients",
+indices = [Index(value = ["name"], unique = true)])
 class IngredientEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int?,
