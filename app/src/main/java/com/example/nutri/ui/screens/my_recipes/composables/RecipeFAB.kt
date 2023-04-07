24e33ccdf32de62a4a83cae0b4f53bc79ec18048
Mary Soproniuk <m.soproniuk@gmail.com>
2023-04-07 13:50:52 +0300

    fix(cosmetic): changed RecipeFAB.kt to ExtendedFAB with textLabel

--- app/src/main/java/com/example/nutri/ui/screens/my_recipes/composables/RecipeFAB.kt
+++ app/src/main/java/com/example/nutri/ui/screens/my_recipes/composables/RecipeFAB.kt
@@ -1,32 +1,59 @@
 package com.example.nutri.ui.screens.my_recipes.composables
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.FloatingActionButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nutri.R
+import com.example.nutri.ui.theme.NutriTheme
 
 @Composable
 fun RecipeFAB(
     goToScreen: () -> Unit
 ){
-    FloatingActionButton(onClick = { goToScreen() },
-        modifier = Modifier.size(56.dp),
+    ExtendedFloatingActionButton(
+        onClick = { goToScreen()},
+        text = { Text(text = "New Recipe", fontSize = MaterialTheme.typography.subtitle2.fontSize) },
         backgroundColor = MaterialTheme.colors.primary,
-        elevation = FloatingActionButtonDefaults.elevation(4.dp)
-    ){
-        Icon(
-            ImageVector.vectorResource(id = R.drawable.add48px),
-            contentDescription = "AddFAB",
-            modifier = Modifier.size(24.dp),
-            tint = Color.White
-        )
+        elevation = FloatingActionButtonDefaults.elevation(4.dp),
+        icon = {
+            Icon(
+                ImageVector.vectorResource(id = R.drawable.add48px),
+                contentDescription = "AddFAB",
+                modifier = Modifier.size(24.dp),
+                tint = Color.White
+            )
+        }
+    )
+}
+
+@Composable
+fun RecipeFABTest(){
+    ExtendedFloatingActionButton(
+        onClick = { },
+        text = { Text(text = "New Recipe", fontSize = MaterialTheme.typography.subtitle2.fontSize) },
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = FloatingActionButtonDefaults.elevation(4.dp),
+        icon = {
+            Icon(
+                ImageVector.vectorResource(id = R.drawable.add48px),
+                contentDescription = "AddFAB",
+                modifier = Modifier.size(24.dp),
+                tint = Color.White
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+fun RecipeFABPreview(){
+    NutriTheme {
+        RecipeFABTest()
     }
 }
