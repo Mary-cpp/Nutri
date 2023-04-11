37f4f9e70ecf3d3b330a744737a331f0b47efc36
Mary Soproniuk <m.soproniuk@gmail.com>
2023-04-11 15:15:06 +0300

    added Gson type adapter for Retrofit ConverterFactory

--- app/src/main/java/com/example/nutri/data/recipe/remote/repository/ApiGatewayImpl.kt
+++ app/src/main/java/com/example/nutri/data/recipe/remote/repository/ApiGatewayImpl.kt
@@ -1,7 +1,10 @@
 package com.example.nutri.data.recipe.remote.repository
 
 import com.example.nutri.data.api.EdamamService
+import com.example.nutri.data.recipe.remote.dto.nutrients.BaseNutrient
+import com.example.nutri.data.recipe.remote.dto.nutrients.BaseNutrientInstanceCreator
 import com.example.nutri.domain.recipes.model.Recipe
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
@@ -15,10 +18,16 @@ class ApiGatewayImpl @Inject constructor() : ApiGateway {
     private val appId = "c4784311"
     private val appKey = "ea0d71aa81a3a366d9d7cc58783563ef"
 
+    private val gson = GsonBuilder()
+        .registerTypeAdapter(
+            BaseNutrient::class.java,
+            BaseNutrientInstanceCreator()
+        ).create()
+
     private fun client(): EdamamService =
         Retrofit.Builder()
             .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(EdamamService::class.java)
 
