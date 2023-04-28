2aeabd981102f206aa620778f247128038b4560b
Mary Soproniuk <m.soproniuk@gmail.com>
2023-04-28 13:56:35 +0300

    fix(notifications): changed triggerTimeInMillis on triggerTimeHours and *Minutes in NotificationsHandler.kt

--- app/src/main/java/com/example/nutri/core/NotificationsHandler.kt
+++ app/src/main/java/com/example/nutri/core/NotificationsHandler.kt
@@ -5,7 +5,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.nutri.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -17,6 +19,9 @@ class NotificationsHandler @Inject constructor(
     private val alarmManager: AlarmManager by lazy { context.getSystemService(Context.ALARM_SERVICE) as AlarmManager}
     private val alarmIntent : Intent by lazy { Intent(context, AlarmReceiver::class.java) }
 
+    private val tag = this::class.java.simpleName
+
+    @RequiresApi(Build.VERSION_CODES.S)
     fun setDefaultAlarms(){
         val contextWrapper = ContextWrapper(context)
 
@@ -28,12 +33,12 @@ class NotificationsHandler @Inject constructor(
                 putExtra("notification_description", contextWrapper.getString(notification.description))
             }
 
-            val pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_IMMUTABLE)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
             notification.intent = pendingIntent
 
             alarmManager.setInexactRepeating(
                 AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                notification.triggerTimeInMillis,
+                getTimeInMillis(notification.triggerTimeHours, notification.triggerTimeMinutes),
                 AlarmManager.INTERVAL_DAY,
                 notification.intent
             ).apply {
@@ -42,15 +47,48 @@ class NotificationsHandler @Inject constructor(
         }
     }
 
-    fun setAlarm(timeInMillis : Long, notification : NotificationType){
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun enableAlarms(){
+        val contextWrapper = ContextWrapper(context)
+
+        NotificationType.values().forEach { notification ->
+            alarmIntent.apply {
+                context
+                action = context.getString(R.string.notification_action)
+                putExtra(
+                    "notification_title",
+                    contextWrapper.resources.getString(notification.title)
+                )
+                putExtra(
+                    "notification_description",
+                    contextWrapper.getString(notification.description)
+                )
+            }
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+            notification.intent = pendingIntent
+
+            alarmManager.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                getTimeInMillis(notification.triggerTimeHours, notification.triggerTimeMinutes),
+                AlarmManager.INTERVAL_DAY,
+                notification.intent
+            ).apply {
+                Log.w("ALARM MANAGER", "Enabled ${notification.text} alarm")
+            }
+        }
+    }
+
+    fun setAlarm(
+        notification : NotificationType,
+    ){
         cancelAlarm(notification)
-        notification.triggerTimeInMillis = timeInMillis
         alarmManager.setInexactRepeating(
             AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            notification.triggerTimeInMillis,
+            getTimeInMillis(notification.triggerTimeHours, notification.triggerTimeMinutes),
             1800*1000,
             notification.intent
         )
+        Log.i(tag, "Changed ${notification.text} notification time on ${notification.triggerTimeHours}")
     }
 
     fun cancelAlarm(notification : NotificationType)
@@ -63,41 +101,46 @@ class NotificationsHandler @Inject constructor(
     }
 }
 
+fun getTimeInMillis(hours: Int, minutes: Int) : Long{
+    return (hours * 3600 * 1000 + minutes * 60 * 1000).toLong()
+}
+
 enum class NotificationType(
     val text: String,
     val title: Int,
     val description: Int,
-    var triggerTimeInMillis: Long,
-    var intent: PendingIntent? = null
+    var intent: PendingIntent? = null,
+    var triggerTimeHours: Int,
+    var triggerTimeMinutes: Int = 0,
 ){
     BREAKFAST(
         text = "Breakfast",
         title = R.string.breakfast_notification_title,
         description = R.string.breakfast_notification_description,
-        triggerTimeInMillis = 9*3600*1000
+        triggerTimeHours = 9,
     ),
     LUNCH(
         text = "Lunch",
         title = R.string.lunch_notification_title,
         description = R.string.lunch_notification_description,
-        triggerTimeInMillis = 14*3600*1000
+        triggerTimeHours = 14,
     ),
     DINNER(
         text = "Dinner",
         title = R.string.dinner_notification_title,
         description = R.string.dinner_notification_description,
-        triggerTimeInMillis = 18*3600*1000
+        triggerTimeHours = 18
     ),
     BRUNCH(
         text = "Brunch",
         title = R.string.brunch_notification_title,
         description = R.string.brunch_notification_description,
-        triggerTimeInMillis = 12*3600*1000
+        triggerTimeHours = 12,
     ),
     WATER(
         text = "Water",
         title = R.string.water_notification_title,
         description = R.string.water_notification_description,
-        triggerTimeInMillis = 9*3600*1000
+        triggerTimeHours = 9
     )
 }
