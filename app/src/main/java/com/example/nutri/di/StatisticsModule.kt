613eb5e5f476760100478e89ce72e63ce57207ea
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-02 15:46:18 +0300

    feat(di): added di

--- app/src/main/java/com/example/nutri/di/StatisticsModule.kt
+++ app/src/main/java/com/example/nutri/di/StatisticsModule.kt
@@ -3,8 +3,8 @@ package com.example.nutri.di
 import com.example.nutri.data.database.RecipeDatabase
 import com.example.nutri.data.statistics.repository.MealDataBaseGateway
 import com.example.nutri.data.statistics.repository.MealDatabaseGatewayImpl
-import com.example.nutri.domain.statistics.MealInteractor
-import com.example.nutri.domain.statistics.MealInteractorImpl
+import com.example.nutri.data.statistics.repository.WaterDatabaseGatewayImpl
+import com.example.nutri.domain.statistics.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +14,7 @@ import dagger.hilt.components.SingletonComponent
 @Module
 object StatisticsModule {
 
-    @Provides()
+    @Provides
     fun provideUseCase(
         dataBaseGateway: MealDataBaseGateway
     ) : MealInteractor
@@ -26,4 +26,16 @@ object StatisticsModule {
         database: RecipeDatabase
     ) : MealDataBaseGateway
     = MealDatabaseGatewayImpl(database = database)
+
+    @Provides
+    fun provideWaterDatabaseGateway(
+        database: RecipeDatabase
+    ) : WaterDatabaseGateway
+    = WaterDatabaseGatewayImpl(database = database)
+
+    @Provides
+    fun provideWaterUseCaseInteractor(
+        dataBaseGateway: WaterDatabaseGateway
+    ) : WaterUseCaseInteractor
+    = WaterUseCaseInteractorImpl(db = dataBaseGateway)
 }
