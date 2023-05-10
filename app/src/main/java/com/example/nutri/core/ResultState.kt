50e4c49bbed935f0202f3feec529fa0def7e7872
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-10 12:18:18 +0300

    fix: removed loading from ResultState.kt

--- app/src/main/java/com/example/nutri/core/ResultState.kt
+++ app/src/main/java/com/example/nutri/core/ResultState.kt
@@ -3,7 +3,6 @@ package com.example.nutri.core
 sealed class ResultState<out R>{
     data class Success<out T>(val value: T) : ResultState<T>()
     data class Error(val exception: Throwable) : ResultState<Nothing>()
-    object Loading : ResultState<Nothing>()
 }
 val <T> ResultState<T>.data : T?
     get() = (this as ResultState.Success).data
