9294f2d2da7ad0187a85947f072f9959fce4d4fb
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-11 14:02:25 +0300

    fix: added back loading to ResultState.kt :)

--- app/src/main/java/com/example/nutri/core/ResultState.kt
+++ app/src/main/java/com/example/nutri/core/ResultState.kt
@@ -2,7 +2,8 @@ package com.example.nutri.core
 
 sealed class ResultState<out R>{
     data class Success<out T>(val value: T) : ResultState<T>()
+    object Loading : ResultState<Nothing>()
     data class Error(val exception: Throwable) : ResultState<Nothing>()
 }
-val <T> ResultState<T>.data : T?
-    get() = (this as ResultState.Success).data
+val <T> ResultState<T>.data : T
+    get() = (this as ResultState.Success).value
