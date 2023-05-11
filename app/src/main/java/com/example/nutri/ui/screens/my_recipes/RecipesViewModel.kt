62a5c9a1e0ae547ed681d3146b0a4e9998c186f6
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-11 17:36:27 +0300

    fix: updated presentation logic for Flow usage

--- app/src/main/java/com/example/nutri/ui/screens/my_recipes/RecipesViewModel.kt
+++ app/src/main/java/com/example/nutri/ui/screens/my_recipes/RecipesViewModel.kt
@@ -6,11 +6,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewModelScope
+import com.example.nutri.core.ResultState
+import com.example.nutri.core.data
 import com.example.nutri.domain.recipes.interactor.LocalRecipesInteractor
 import com.example.nutri.domain.recipes.model.Recipe
 import com.example.nutri.ui.navigation.NavControllerHolder
 import com.example.nutri.ui.navigation.NavigationViewModel
+import com.example.nutri.ui.screens.my_recipes.composables.SortAction
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,6 +25,11 @@ class RecipesViewModel @Inject constructor(
 ) : NavigationViewModel(navControllerProvider), DefaultLifecycleObserver {
 
     val recipeList : MutableState<List<Recipe>> = mutableStateOf(listOf())
+    val isDataLoading : MutableState<Boolean> = mutableStateOf(true)
+
+    private var _recipeListState : MutableStateFlow<ResultState<List<Recipe>>>
+    = MutableStateFlow(ResultState.Loading)
+    val recipeListState : StateFlow<ResultState<List<Recipe>>> = _recipeListState
 
     private val tag = "MyRecipesViewModel"
 
@@ -29,19 +38,28 @@ class RecipesViewModel @Inject constructor(
     }
 
     private fun getSavedRecipes() = viewModelScope.launch{
-
         Log.d(tag, "MyRecipes Screen Loaded        START")
 
-        recipeList.value = useCase.receiveRecipes()
-
-        Log.d(tag, "MyRecipes Screen Loaded         END")
+        useCase.receiveRecipes().collect{ result ->
+            _recipeListState.value = result
+            when(recipeListState.value){
+                is ResultState.Success -> {
+                    recipeList.value = recipeListState.value.data
+                    isDataLoading.value = false
+                }
+                is ResultState.Loading -> isDataLoading.value = true
+                is ResultState.Error -> {}
+            }
+        }
     }
 
-    fun sortRecipes(param: Int) : List<Recipe>{
-        return when(param){
-            1 -> recipeList.value.sortedBy { it.name }
-            2 -> recipeList.value.sortedBy { it.calories }
-            else -> recipeList.value
+    fun onSortListSelectedItemChanged(param: SortAction) {
+        viewModelScope.launch {
+            useCase.sortRecipesBy(
+                sortAction = param,
+                list = recipeList.value
+            )
+                .collect { sortResult -> recipeList.value = sortResult }
         }
     }
 }
