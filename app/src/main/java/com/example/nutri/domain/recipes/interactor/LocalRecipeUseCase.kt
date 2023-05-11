1d8516e519b297dec5e3b98f0a227ce5e84ac97f
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-11 14:04:44 +0300

    fix: use case fetches Flow from repository and directs it to VM + moved data sort to use case

--- app/src/main/java/com/example/nutri/domain/recipes/interactor/LocalRecipeUseCase.kt
+++ app/src/main/java/com/example/nutri/domain/recipes/interactor/LocalRecipeUseCase.kt
@@ -3,7 +3,11 @@ package com.example.nutri.domain.recipes.interactor
 import com.example.nutri.core.ResultState
 import com.example.nutri.data.recipe.local.repository.DataBaseGateway
 import com.example.nutri.domain.recipes.model.Recipe
+import com.example.nutri.ui.screens.my_recipes.composables.SortAction
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class LocalRecipeUseCase @Inject constructor(
@@ -14,7 +18,7 @@ class LocalRecipeUseCase @Inject constructor(
         return db.saveToLocal(recipe, recipeName)
     }
 
-    override suspend fun receiveRecipes() : List<Recipe>{
+    override suspend fun receiveRecipes() : Flow<ResultState<List<Recipe>>> {
         return db.getLocalRecipesList()
     }
 
@@ -30,4 +34,20 @@ class LocalRecipeUseCase @Inject constructor(
         return db.getRecipesWithNameLike(name)
     }
 
+    override fun sortRecipesBy(sortAction: SortAction, list: List<Recipe>) : Flow<List<Recipe>> {
+        return when(sortAction){
+            SortAction.NAME_ASC -> flow {
+                emit(list.sortedBy { it.name })
+            }.flowOn(Dispatchers.Unconfined)
+            SortAction.NAME_DESC -> flow {
+                emit(list.sortedByDescending { it.name })
+            }.flowOn(Dispatchers.Unconfined)
+            SortAction.CALORIES_ASC -> flow {
+                emit(list.sortedBy { it.calories })
+            }.flowOn(Dispatchers.Unconfined)
+            SortAction.CALORIES_DESC -> flow {
+                emit(list.sortedByDescending { it.calories })
+            }.flowOn(Dispatchers.Unconfined)
+        }
+    }
 }
--- app/src/main/java/com/example/nutri/domain/recipes/interactor/LocalRecipesInteractor.kt
+++ app/src/main/java/com/example/nutri/domain/recipes/interactor/LocalRecipesInteractor.kt
@@ -2,13 +2,15 @@ package com.example.nutri.domain.recipes.interactor
 
 import com.example.nutri.core.ResultState
 import com.example.nutri.domain.recipes.model.Recipe
+import com.example.nutri.ui.screens.my_recipes.composables.SortAction
 import kotlinx.coroutines.flow.Flow
 
 interface LocalRecipesInteractor {
 
     suspend fun saveRecipe(recipe: Recipe, recipeName: String): String
-    suspend fun receiveRecipes(): List<Recipe>
+    suspend fun receiveRecipes(): Flow<ResultState<List<Recipe>>>
     suspend fun getCommonRecipe(recipeId : String) : Flow<ResultState<Recipe>>
     suspend fun deleteRecipe(recipe:Recipe)
     suspend fun getRecipesLike(name: String): List<Recipe>?
+    fun sortRecipesBy(sortAction: SortAction, list: List<Recipe>) : Flow<List<Recipe>>
 }
