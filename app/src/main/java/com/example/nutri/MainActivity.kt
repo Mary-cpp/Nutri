c30330e7315d60768dd3de6c73ee39efe06e9c44
Mary Soproniuk <m.soproniuk@gmail.com>
2023-05-15 13:33:17 +0300

    moved services and permission interaction to NutriApplication.kt

--- app/src/main/java/com/example/nutri/MainActivity.kt
+++ app/src/main/java/com/example/nutri/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.example.nutri
 
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material.Scaffold
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import com.example.nutri.core.NotificationWork
-import com.example.nutri.core.NotificationsHandler
 import com.example.nutri.ui.navigation.BottomNavigationBar
 import com.example.nutri.ui.navigation.NavControllerHolder
 import com.example.nutri.ui.navigation.NavigationGraph
-import com.example.nutri.ui.screens.configs.NUTRI_PREFERENCES
 import com.example.nutri.ui.theme.NutriTheme
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -35,24 +25,8 @@ class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val notificationPermission = android.Manifest.permission.POST_NOTIFICATIONS
-        if (ContextCompat.checkSelfPermission(this, notificationPermission) != PackageManager.PERMISSION_GRANTED){
-            Log.w(this::class.java.simpleName, "Notifications permission denied")
-            requestPermissions(arrayOf(notificationPermission), 112)
-        }
-
-        val sp = getSharedPreferences(NUTRI_PREFERENCES, Context.MODE_PRIVATE)
-
-        if (sp.getBoolean("isFirstRun", true)){
-            NotificationsHandler(context = this).enableAlarms()
-            sp.edit().putBoolean("isFirstRun", false).apply()
-        }
-        val notificationsWorkRequest = PeriodicWorkRequestBuilder<NotificationWork>(24,TimeUnit.HOURS).build()
-        WorkManager.getInstance(this).enqueue(notificationsWorkRequest)
-
         setContent {
             val navController: NavHostController = rememberNavController()
-
             navControllerHolder.navController = navController
 
             NutriTheme {
